fef3ca6ba093aeb00d6345108bf9d3c5cd4cbc53
Abhishek Deshkar <deshkarabhishek@gmail.com>
2018-10-13 21:48:54 +0530

    First commit

---
+++ molap.php
@@ -0,0 +1,345 @@
+<?php
+
+    $phone_model = array( 
+
+        array('model_id' => 1,'model_name' => 'S9','model_cost' => 57000,'model_type' => '4G','brand_name' => 'Samsung'),
+        array('model_id' => 2,'model_name' => 'Nexus 6P','model_cost' => 40000,'model_type' => '4G','brand_name' => 'huawei'),
+        array('model_id' => 3,'model_name' => 'Galaxy ACE','model_cost' => 12000,'model_type' => '3G','brand_name' => 'Samsung'),
+        array('model_id' => 4,'model_name' => 'N700','model_cost' => 30000,'model_type' => '4G','brand_name' => 'Nokia'),
+        array('model_id' => 5,'model_name' => 'One Plus 3','model_cost' => 27000,'model_type' => '4G','brand_name' => 'One Plus'),
+        array('model_id' => 6,'model_name' => 'iPhone 6S','model_cost' => 60000,'model_type' => '4G','brand_name' => 'Apple'),
+        array('model_id' => 7,'model_name' => 'Pixel','model_cost' => 80000,'model_type' => '4G','brand_name' => 'Google'),
+        array('model_id' => 8,'model_name' => 'Moto X','model_cost' => 22000,'model_type' => '4G','brand_name' => 'Motorola'),
+        array('model_id' => 9,'model_name' => 'Asus ROG','model_cost' => 71000,'model_type' => '4G','brand_name' => 'Asus'),
+        array('model_id' => 10,'model_name' => 'Lenovo K5 Note','model_cost' => 15000,'model_type' => '4G','brand_name' => 'Lenovo'),
+
+
+    );
+
+    $Location	= array(
+
+            array('city_id' => 1,'city_name' => 'Pune'),
+            array('city_id' => 2,'city_name' => 'Mumbai'),
+            array('city_id' => 3,'city_name' => 'Rajkot'),
+            array('city_id' => 4,'city_name' => 'Surat'),
+            array('city_id' => 5,'city_name' => 'Jaipur'),
+            array('city_id' => 6,'city_name' => 'Udaipur'),
+            array('city_id' => 7,'city_name' => 'Kolhapur'),
+            array('city_id' => 8,'city_name' => 'Ahmedabad'),
+            array('city_id' => 9,'city_name' => 'Kolkata'),
+            array('city_id' => 10,'city_name' => 'Bangalore')
+
+    );
+
+    $Customer	= array(
+
+        array('cust_id' => 1,'city_id'=>1,'cust_name'=>'Aeet','model_id'=>1,'date' =>1,'month'=>1,'year'=>2015),
+        array('cust_id' => 2,'city_id'=>1,'cust_name'=>'Beet','model_id'=>2,'date' =>2,'month'=>1,'year'=>2015),
+        array('cust_id' => 3,'city_id'=>1,'cust_name'=>'Ceet','model_id'=>3,'date' =>3,'month'=>2,'year'=>2015),
+        array('cust_id' => 4,'city_id'=>4,'cust_name'=>'Deet','model_id'=>2,'date' =>4,'month'=>2,'year'=>2015),
+        array('cust_id' => 5,'city_id'=>5,'cust_name'=>'Eeet','model_id'=>2,'date' =>5,'month'=>3,'year'=>2015),
+        array('cust_id' => 6,'city_id'=>6,'cust_name'=>'Feet','model_id'=>5,'date' =>6,'month'=>3,'year'=>2015),
+        array('cust_id' => 7,'city_id'=>7,'cust_name'=>'Geet','model_id'=>5,'date' =>7,'month'=>4,'year'=>2015),
+        array('cust_id' => 8,'city_id'=>8,'cust_name'=>'Heet','model_id'=>1,'date' =>8,'month'=>4,'year'=>2015),
+        array('cust_id' => 9,'city_id'=>9,'cust_name'=>'Ieet','model_id'=>9,'date' =>9,'month'=>5,'year'=>2015),
+        array('cust_id' => 10,'city_id'=>4,'cust_name'=>'Jeet','model_id'=>4,'date' =>10,'month'=>5,'year'=>2015),
+        array('cust_id' => 11,'city_id'=>4,'cust_name'=>'Keet','model_id'=>6,'date' =>11,'month'=>6,'year'=>2015),
+        array('cust_id' => 12,'city_id'=>2,'cust_name'=>'Leet','model_id'=>8,'date' =>12,'month'=>6,'year'=>2015),
+        array('cust_id' => 13,'city_id'=>2,'cust_name'=>'Meet','model_id'=>1,'date' =>13,'month'=>7,'year'=>2015),
+        array('cust_id' => 14,'city_id'=>8,'cust_name'=>'Neet','model_id'=>8,'date' =>14,'month'=>7,'year'=>2015),
+        array('cust_id' => 15,'city_id'=>4,'cust_name'=>'Oeet','model_id'=>4,'date' =>15,'month'=>8,'year'=>2015),
+        array('cust_id' => 16,'city_id'=>7,'cust_name'=>'Peet','model_id'=>5,'date' =>16,'month'=>8,'year'=>2015),
+        array('cust_id' => 17,'city_id'=>8,'cust_name'=>'Qeet','model_id'=>7,'date' =>17,'month'=>9,'year'=>2015),
+        array('cust_id' => 18,'city_id'=>5,'cust_name'=>'Reet','model_id'=>10,'date' =>18,'month'=>9,'year'=>2015),
+        array('cust_id' => 19,'city_id'=>7,'cust_name'=>'Seet','model_id'=>9,'date' =>19,'month'=>10,'year'=>2015),
+        array('cust_id' => 20,'city_id'=>3,'cust_name'=>'Teet','model_id'=>4,'date' =>20,'month'=>10,'year'=>2015),
+        array('cust_id' => 21,'city_id'=>5,'cust_name'=>'Ueet','model_id'=>3,'date' =>21,'month'=>11,'year'=>2015),
+        array('cust_id' => 22,'city_id'=>8,'cust_name'=>'Veet','model_id'=>7,'date' =>22,'month'=>11,'year'=>2015),
+        array('cust_id' => 23,'city_id'=>3,'cust_name'=>'Weet','model_id'=>6,'date' =>23,'month'=>12,'year'=>2015),
+        array('cust_id' => 24,'city_id'=>8,'cust_name'=>'Xeet','model_id'=>3,'date' =>24,'month'=>12,'year'=>2015),
+        array('cust_id' => 25,'city_id'=>6,'cust_name'=>'Yeet','model_id'=>2,'date' =>25,'month'=>5,'year'=>2015),
+        array('cust_id' => 26,'city_id'=>9,'cust_name'=>'Zeet','model_id'=>1,'date' =>26,'month'=>5,'year'=>2015),
+    );
+
+    //Code to generate Fact table. 
+    $Quarter = array('1','2','3','4');	
+    foreach ($Customer as $CustVal) 
+    {
+        $CustomerID = $CustVal['cust_id'];
+        $CityID 	= $CustVal['city_id'];
+        $ModelID 	= $CustVal['model_id'];
+        $CityName   = "";
+        $ModelName  = "";
+        $BrandName  = "";
+        $ModelCost  = "";
+
+        foreach ($Location as $LocationData)
+        {
+            if($LocationData['city_id'] == $CityID)
+            {
+                $CityName = $LocationData['city_name'];
+                break;
+            }
+        }
+
+        foreach ($phone_model as $PhoneData)
+        {
+            if($PhoneData['model_id'] == $ModelID)
+            {
+                $ModelName = $PhoneData['model_name'];
+                $BrandName = $PhoneData['brand_name'];
+                $ModelCost = $PhoneData['model_cost'];
+
+                break;
+            }
+        }
+
+        // echo "array('city_id' => ".$CityID.", 'city_name' => '".$CityName."', 'model_id'=>".$ModelID.", 'model_name'=>'".$ModelName."','brand_name' =>'".$BrandName."' ,'model_cost' => ".$ModelCost.",'date'=>".$CustVal['date'].",'month' => ".$CustVal['month'].",'year'=>'2015','cust_id' =>".$CustomerID." ),"."\n";
+    }		
+
+    $FactTable = array(
+
+        array('city_id' => 1, 'city_name' => 'Pune', 'model_id'=>1, 'model_name'=>'S9','brand_name' =>'Samsung' ,'model_cost' => 57000,'date'=>23,'month' => 11,'year'=>'2015','cust_id' =>1 ),
+		array('city_id' => 1, 'city_name' => 'Pune', 'model_id'=>2, 'model_name'=>'Nexus 6P','brand_name' =>'huawei' ,'model_cost' => 40000,'date'=>8,'month' => 1,'year'=>'2015','cust_id' =>2 ),
+		array('city_id' => 1, 'city_name' => 'Pune', 'model_id'=>3, 'model_name'=>'Galaxy ACE','brand_name' =>'Samsung' ,'model_cost' => 12000,'date'=>2,'month' => 8,'year'=>'2015','cust_id' =>3 ),
+		array('city_id' => 4, 'city_name' => 'Surat', 'model_id'=>2, 'model_name'=>'Nexus 6P','brand_name' =>'huawei' ,'model_cost' => 40000,'date'=>20,'month' => 10,'year'=>'2015','cust_id' =>4 ),
+		array('city_id' => 5, 'city_name' => 'Jaipur', 'model_id'=>2, 'model_name'=>'Nexus 6P','brand_name' =>'huawei' ,'model_cost' => 40000,'date'=>3,'month' => 5,'year'=>'2015','cust_id' =>5 ),
+		array('city_id' => 6, 'city_name' => 'Udaipur', 'model_id'=>5, 'model_name'=>'One Plus 3','brand_name' =>'One Plus' ,'model_cost' => 27000,'date'=>11,'month' => 2,'year'=>'2015','cust_id' =>6 ),
+		array('city_id' => 7, 'city_name' => 'Kolhapur', 'model_id'=>5, 'model_name'=>'One Plus 3','brand_name' =>'One Plus' ,'model_cost' => 27000,'date'=>1,'month' => 9,'year'=>'2015','cust_id' =>7 ),
+		array('city_id' => 8, 'city_name' => 'Ahmedabad', 'model_id'=>1, 'model_name'=>'S9','brand_name' =>'Samsung' ,'model_cost' => 57000,'date'=>18,'month' => 10,'year'=>'2015','cust_id' =>8 ),
+		array('city_id' => 9, 'city_name' => 'Kolkata', 'model_id'=>9, 'model_name'=>'Asus ROG','brand_name' =>'Asus' ,'model_cost' => 71000,'date'=>1,'month' => 6,'year'=>'2015','cust_id' =>9 ),
+		array('city_id' => 4, 'city_name' => 'Surat', 'model_id'=>4, 'model_name'=>'N700','brand_name' =>'Nokia' ,'model_cost' => 30000,'date'=>1,'month' => 12,'year'=>'2015','cust_id' =>10 ),
+		array('city_id' => 4, 'city_name' => 'Surat', 'model_id'=>6, 'model_name'=>'iPhone 6S','brand_name' =>'Apple' ,'model_cost' => 60000,'date'=>13,'month' => 11,'year'=>'2015','cust_id' =>11 ),
+		array('city_id' => 2, 'city_name' => 'Mumbai', 'model_id'=>8, 'model_name'=>'Moto X','brand_name' =>'Motorola' ,'model_cost' => 22000,'date'=>8,'month' => 10,'year'=>'2015','cust_id' =>12 ),
+		array('city_id' => 2, 'city_name' => 'Mumbai', 'model_id'=>1, 'model_name'=>'S9','brand_name' =>'Samsung' ,'model_cost' => 57000,'date'=>28,'month' => 4,'year'=>'2015','cust_id' =>13 ),
+		array('city_id' => 8, 'city_name' => 'Ahmedabad', 'model_id'=>8, 'model_name'=>'Moto X','brand_name' =>'Motorola' ,'model_cost' => 22000,'date'=>19,'month' => 7,'year'=>'2015','cust_id' =>14 ),
+		array('city_id' => 4, 'city_name' => 'Surat', 'model_id'=>4, 'model_name'=>'N700','brand_name' =>'Nokia' ,'model_cost' => 30000,'date'=>5,'month' => 10,'year'=>'2015','cust_id' =>15 ),
+		array('city_id' => 7, 'city_name' => 'Kolhapur', 'model_id'=>5, 'model_name'=>'One Plus 3','brand_name' =>'One Plus' ,'model_cost' => 27000,'date'=>20,'month' => 3,'year'=>'2015','cust_id' =>16 ),
+		array('city_id' => 8, 'city_name' => 'Ahmedabad', 'model_id'=>7, 'model_name'=>'Pixel','brand_name' =>'Google' ,'model_cost' => 80000,'date'=>2,'month' => 7,'year'=>'2015','cust_id' =>17 ),
+		array('city_id' => 5, 'city_name' => 'Jaipur', 'model_id'=>10, 'model_name'=>'Lenovo K5 Note','brand_name' =>'Lenovo' ,'model_cost' => 15000,'date'=>22,'month' => 12,'year'=>'2015','cust_id' =>18 ),
+		array('city_id' => 7, 'city_name' => 'Kolhapur', 'model_id'=>9, 'model_name'=>'Asus ROG','brand_name' =>'Asus' ,'model_cost' => 71000,'date'=>27,'month' => 8,'year'=>'2015','cust_id' =>19 ),
+		array('city_id' => 3, 'city_name' => 'Rajkot', 'model_id'=>4, 'model_name'=>'N700','brand_name' =>'Nokia' ,'model_cost' => 30000,'date'=>23,'month' => 8,'year'=>'2015','cust_id' =>20 ),
+		array('city_id' => 5, 'city_name' => 'Jaipur', 'model_id'=>3, 'model_name'=>'Galaxy ACE','brand_name' =>'Samsung' ,'model_cost' => 12000,'date'=>26,'month' => 7,'year'=>'2015','cust_id' =>21 ),
+		array('city_id' => 8, 'city_name' => 'Ahmedabad', 'model_id'=>7, 'model_name'=>'Pixel','brand_name' =>'Google' ,'model_cost' => 80000,'date'=>11,'month' => 12,'year'=>'2015','cust_id' =>22 ),
+		array('city_id' => 3, 'city_name' => 'Rajkot', 'model_id'=>6, 'model_name'=>'iPhone 6S','brand_name' =>'Apple' ,'model_cost' => 60000,'date'=>7,'month' => 11,'year'=>'2015','cust_id' =>23 ),
+		array('city_id' => 8, 'city_name' => 'Ahmedabad', 'model_id'=>3, 'model_name'=>'Galaxy ACE','brand_name' =>'Samsung' ,'model_cost' => 12000,'date'=>8,'month' => 12,'year'=>'2015','cust_id' =>24 ),
+		array('city_id' => 6, 'city_name' => 'Udaipur', 'model_id'=>2, 'model_name'=>'Nexus 6P','brand_name' =>'huawei' ,'model_cost' => 40000,'date'=>26,'month' => 3,'year'=>'2015','cust_id' =>25 ),
+		array('city_id' => 9, 'city_name' => 'Kolkata', 'model_id'=>1, 'model_name'=>'S9','brand_name' =>'Samsung' ,'model_cost' => 57000,'date'=>22,'month' => 9,'year'=>'2015','cust_id' =>26 ),
+
+    );
+		
+    $SliceCube 	= Slice($Customer);
+
+    $DiceCube 	=  Dice($Location,$Customer);
+
+    // echo print_r(RollUpOperation($FactTable));
+    // echo print_r(Slice($Customer));
+    // echo print_r(Dice($Location,$Customer));
+    
+
+	echo DrillDownOperation($FactTable);
+	
+
+	function DrillDownOperation($FactTable)
+	{
+
+		$JanProfit 		= 0;
+		$FebProfit		= 0;
+		$MarchProfit	= 0;
+		$AprilProfit	= 0;
+		$MayProfit		= 0;
+		$JuneProfit		= 0;
+		$JulyProfit 	= 0;
+		$AugProfit		= 0;
+		$SeptProfit 	= 0;
+		$OctoProfit		= 0;
+		$NovProfit		= 0;
+		$DecProfit		= 0;
+
+		//Profit by monthly.
+		foreach ($FactTable as $FactData)
+		{
+			switch ($FactData['month']) 
+			{
+				
+				case '1':
+					$JanProfit = $JanProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '2':
+					$FebProfit = $FebProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '3':
+					$MarchProfit = $MarchProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '4':
+					$AprilProfit = $AprilProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '5':
+					$MayProfit = $MayProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '6':
+					$JuneProfit = $JuneProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '7':
+					$JulyProfit = $JulyProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '8':
+					$AugProfit = $AugProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '9':
+					$SeptProfit = $SeptProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '10':
+					$OctoProfit = $OctoProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '11':
+					$NovProfit = $NovProfit + (int)$FactData['model_cost'];
+				break;
+
+				case '12':
+					$DecProfit = $DecProfit + (int)$FactData['model_cost'];
+				break;
+
+
+			}
+		}
+
+
+		$ShowProfitByMonths = "January Profit : $JanProfit  INR \nFebruary Profit : $FebProfit  INR \nMarch Profit : $MarchProfit  INR \nApril Profit : $AprilProfit  INR \nMay Profit : $MayProfit  INR \nJune Profit : $JuneProfit  INR \nJuly Profit : $JulyProfit  INR \nAugust Profit : $AugProfit  INR \nSeptember Profit : $SeptProfit  INR \nOctober Profit : $OctoProfit  INR \nNovember Profit : $NovProfit  INR \nDecember Profit : $DecProfit  INR \n";
+
+		return $ShowProfitByMonths;
+
+	}
+
+
+	function RollUpOperation($FactTable)
+	{
+		//State wise profit.
+
+		$KarnatakaProfit = 0;
+
+		//Karnataka
+		foreach($FactTable as $FactData)
+		{
+			if($FactData['city_id'] == 10)
+			{
+				$KarnatakaProfit = $KarnatakaProfit + $FactData['model_cost'];
+			}
+
+		}
+
+		//Maharashtra Profit.
+
+		$MaharashtraProfit = 0;
+
+		foreach($FactTable as $FactData)
+		{
+			if($FactData['city_id'] == 1 || $FactData['city_id'] == 2 || $FactData['city_id'] == 7 )
+			{
+				$MaharashtraProfit = $MaharashtraProfit + $FactData['model_cost'];
+			}
+
+		}
+
+		//Rajasthan Profit
+
+		$RajasthanProfit = 0;
+
+		foreach($FactTable as $FactData)
+		{
+			if($FactData['city_id'] == 5 || $FactData['city_id'] == 6 )
+			{
+				$RajasthanProfit = $RajasthanProfit + $FactData['model_cost'];
+			}
+
+		}
+
+		//Gujarat Profit.
+
+		$GujaratProfit = 0;
+
+		foreach($FactTable as $FactData)
+		{
+			if($FactData['city_id'] == 4 || $FactData['city_id'] == 8 )
+			{
+				$GujaratProfit = $GujaratProfit + $FactData['model_cost'];
+			}
+
+		}
+
+
+		//West Bengal Profit.
+		$WestBengalProfit = 0;
+
+		foreach($FactTable as $FactData)
+		{
+			if($FactData['city_id'] == 9 )
+			{
+				$WestBengalProfit = $WestBengalProfit + $FactData['model_cost'];
+			}
+
+		}
+
+		$PrintProfit = "Karnataka Profit: $KarnatakaProfit INR \nGujarat Profit: $GujaratProfit INR \nMaharashtra Profit: $MaharashtraProfit INR \nRajasthan Profit: $RajasthanProfit INR \nWest Bengal Profit: $WestBengalProfit INR \n ";
+
+		return $PrintProfit;
+
+	}
+
+	function Slice($Customer)
+	{
+		//Performing slice on customer dimension and generating a sub cubeoid.
+		
+		//Customer name and model ID
+
+		$CustomerCityCube = array();
+
+		foreach($Customer as $CustomerData)
+		{
+			$CustomerCityCube[] = array( "cust_name" => $CustomerData['cust_name'], 'model_id' => $CustomerData['model_id'] );
+		}
+
+		return $CustomerCityCube;
+
+	}
+
+	function Dice($Location,$Customer)
+	{
+		//Performing slice on customer and city dimension and generating a sub cubeoid.
+
+
+		$CustomerCityLocationCube = array();
+		
+		foreach($Customer as $CustomerData)
+		{
+			$TempCity = "";
+
+			foreach($Location as $LocationData)
+			{
+				if($LocationData['city_id'] == $CustomerData['city_id'])
+				{
+					$TempCity = $LocationData['city_name'];
+					break;
+				}
+			}
+
+
+			$CustomerCityLocationCube[] = array( 'cust_name' => $CustomerData['cust_name'], 'city_name' => $TempCity );
+		}
+
+		return $CustomerCityLocationCube;
+
+	}
+
+
+
